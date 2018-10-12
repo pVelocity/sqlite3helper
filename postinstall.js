30f52faa7b9e63902b54a27fd80177914d6125e1
Allen Chung <allen.chung@pvelocity.com>
2018-10-12 17:38:55 -0400

    include postinstall to ensure sqlite3 binaries

---
+++ postinstall.js
@@ -0,0 +1,18 @@
+/*global PV */
+'use strict';
+
+/* jshint strict: true */
+/* jshint node: true */
+/* jshint unused: false */
+
+var fs = require('fs-extra');
+var path = require('path');
+
+function main() {
+    fs.copySync(path.resolve(__dirname, './sqlite3'), path.resolve(__dirname, './node_modules/sqlite3'), {
+        overwrite: false
+    });
+    return;
+}
+
+main();
